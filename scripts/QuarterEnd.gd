af57632fbea982ee44e815083225f7bcc8b00ef3
david8zhang <david8zhang@berkeley.edu>
2025-05-03 16:19:43 -0700

    added end of quarter info modal

---
+++ scripts/QuarterEnd.gd
@@ -0,0 +1,8 @@
+class_name QuarterEnd
+extends Control
+
+@export var box_score_row: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
