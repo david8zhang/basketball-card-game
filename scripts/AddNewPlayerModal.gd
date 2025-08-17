077945dbc363dcc491249243b571ae48d92f3aaa
david8zhang <david8zhang@berkeley.edu>
2025-08-17 12:20:07 -0700

    preview cards that are addable to team as rewards

--- scripts/AddNewPlayerModal.gd
+++ scripts/AddNewPlayerModal.gd
@@ -2,6 +2,7 @@ class_name AddNewPlayerModal
 extends Control
 
 @export var bp_card_scene: PackedScene
+@export var bp_card_preview_scene: PackedScene
 @onready var player_lineup_slots = $VBoxContainer/HBoxContainer as HBoxContainer
 @onready var card_to_pick_container = $VBoxContainer/MarginContainer/ScrollContainer/HBoxContainer2 as HBoxContainer
 @onready var card_cost_too_high = $CardCostTooHigh as Label
@@ -9,8 +10,9 @@ extends Control
 @onready var continue_button = $Continue as Button
 @onready var close_button = $Close as Button
 
-var bp_stat_to_add
-var new_bp_to_add
+var selected_bp_card: BallPlayerCard
+var orig_reward_bp_stat: BallPlayerStats
+var bp_card_preview: BPCardPreview
 
 signal on_add_player(new_lineup)
 
@@ -30,24 +32,35 @@ func _ready() -> void:
 
 func set_bp_stat_to_add(bp_stat: BallPlayerStats):
 	var ball_player_card = bp_card_scene.instantiate() as BallPlayerCard
+	ball_player_card.on_bp_card_clicked.connect(on_click_card)
 	ball_player_card.ball_player_stats = bp_stat
 	card_to_pick_container.add_child(ball_player_card)
-	bp_stat_to_add = bp_stat
-	new_bp_to_add = bp_stat
+	orig_reward_bp_stat = bp_stat
 
 func on_set_bp_card(card_slot: CardSlot):
-	var bp_card = bp_card_scene.instantiate() as BallPlayerCard
-	bp_card.ball_player_stats = bp_stat_to_add
-	var selected_bp_card_cost = bp_stat_to_add.player_cost		
-	var curr_cost_total = get_curr_cost_total()
-	var curr_card_in_slot_cost = card_slot.card_in_slot.ball_player_stats.player_cost
-	if curr_cost_total - curr_card_in_slot_cost + selected_bp_card_cost <= SceneVariables.salary_cap:
-		replace_drafted_bp_card(card_slot.card_in_slot)
-		card_slot.set_bp_card_in_slot(bp_card)
-		update_curr_player_cost_total()
+	if selected_bp_card != null:
+		var bp_card = bp_card_scene.instantiate() as BallPlayerCard
+		bp_card.ball_player_stats = selected_bp_card.ball_player_stats
+		var selected_bp_card_cost = selected_bp_card.ball_player_stats.player_cost		
+		var curr_cost_total = get_curr_cost_total()
+		var curr_card_in_slot_cost = card_slot.card_in_slot.ball_player_stats.player_cost
+		if curr_cost_total - curr_card_in_slot_cost + selected_bp_card_cost <= SceneVariables.salary_cap:
+			replace_drafted_bp_card(card_slot.card_in_slot)
+			card_slot.set_bp_card_in_slot(bp_card)
+			update_curr_player_cost_total()
+		else:
+			show_over_budget_alert()
+		continue_button.visible = can_continue()
 	else:
-		show_over_budget_alert()
-	continue_button.visible = bp_stat_to_add != new_bp_to_add
+		on_click_card(card_slot.card_in_slot, false)
+
+func can_continue():
+	for child in player_lineup_slots.get_children():
+		var slot = child as CardSlot
+		var card_in_slot = slot.card_in_slot as BallPlayerCard
+		if card_in_slot.full_name() == orig_reward_bp_stat.get_full_name():
+			return true
+	return false
 
 func replace_drafted_bp_card(player_to_replace: BallPlayerCard):
 	var bp_card = bp_card_scene.instantiate() as BallPlayerCard
@@ -55,7 +68,6 @@ func replace_drafted_bp_card(player_to_replace: BallPlayerCard):
 	for child in card_to_pick_container.get_children():
 		child.queue_free()
 	card_to_pick_container.add_child(bp_card)
-	bp_stat_to_add = player_to_replace.ball_player_stats
 	var callable = Callable(self, "on_click_card").bind(bp_card)
 	bp_card.button.pressed.connect(callable)
 
@@ -96,3 +108,22 @@ func on_continue():
 
 func on_close():
 	queue_free()
+
+func on_click_card(bp_card, show_select_button = true):
+	bp_card_preview = bp_card_preview_scene.instantiate() as BPCardPreview
+	bp_card_preview.show_select_button = show_select_button
+	bp_card_preview.bp_stat_to_preview = bp_card.ball_player_stats
+	add_child(bp_card_preview)
+	bp_card_preview.on_close.connect(close_bp_card_preview)
+	var callable = Callable(self, "select_bp_card").bind(bp_card)
+	bp_card_preview.on_selected.connect(callable)
+
+func close_bp_card_preview():
+	bp_card_preview.queue_free()
+
+func select_bp_card(bp_card: BallPlayerCard):
+	if selected_bp_card != null:
+		selected_bp_card.disable_highlight()
+	selected_bp_card = bp_card
+	selected_bp_card.enable_highlight()
+	close_bp_card_preview()	
--- scripts/BPCardPreview.gd
+++ scripts/BPCardPreview.gd
@@ -6,19 +6,21 @@ extends Control
 @onready var close_button = $CloseButton as Button
 
 var bp_stat_to_preview: BallPlayerStats
+var show_select_button: bool = true
 
 signal on_selected
 signal on_close
 
 func _ready():
-  var matchup_bp_card = matchup_bp_card_scene.instantiate() as MatchupPlayerCard
-  matchup_bp_card.ball_player_stats = bp_stat_to_preview
-  add_child(matchup_bp_card)
-  close_button.pressed.connect(close)
-  select_button.pressed.connect(select)
+	var matchup_bp_card = matchup_bp_card_scene.instantiate() as MatchupPlayerCard
+	matchup_bp_card.ball_player_stats = bp_stat_to_preview
+	add_child(matchup_bp_card)
+	close_button.pressed.connect(close)
+	select_button.pressed.connect(select)
+	select_button.visible = show_select_button
 
 func close():
-  on_close.emit()
+	on_close.emit()
 
 func select():
-  on_selected.emit()
+	on_selected.emit()
