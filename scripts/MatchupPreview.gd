fa46af97ae0acfd16e63e73fb80f2549786bf6c3
david8zhang <david8zhang@berkeley.edu>
2025-09-23 23:53:09 -0700

    compare bench lineups

--- scripts/MatchupPreview.gd
+++ scripts/MatchupPreview.gd
@@ -3,14 +3,18 @@ extends PanelContainer
 
 @onready var player_lineup_container = $MarginContainer/VBoxContainer/ScrollContainer/PlayerLineupContainer
 @onready var cpu_lineup_container = $MarginContainer/VBoxContainer/ScrollContainer2/CPULineupContainer
+@onready var cpu_lineup_label = $MarginContainer/VBoxContainer/CPULineupLabel
+@onready var player_lineup_label = $MarginContainer/VBoxContainer/PlayerLineupLabel
 @onready var continue_button = $MarginContainer/Button
 @onready var edit_lineups_button = $MarginContainer/Button2
+@onready var compare_lineup_button = $MarginContainer/Button3
 
 @export var bp_card_scene: PackedScene
 @export var bp_card_preview_scene: PackedScene
 
 var selected_bp_card
 var bp_card_preview
+var is_comparing_bench = false
 
 signal on_edit_lineups_clicked
 signal on_continue_clicked
@@ -30,16 +34,25 @@ func _ready() -> void:
 		on_continue_clicked.emit()
 	continue_button.pressed.connect(on_continue)
 
-func update_lineups():
-	display_specific_lineups(SceneVariables.player_team_bp_configs, player_lineup_container, Game.Side.PLAYER)
-	display_specific_lineups(SceneVariables.cpu_team_bp_configs, cpu_lineup_container, Game.Side.CPU)
+	compare_lineup_button.pressed.connect(on_compare_bench)
 
-func display_specific_lineups(team_bp_configs, container, side: Game.Side):
+func update_lineups():
+	display_starter_lineups(SceneVariables.player_team_bp_configs, player_lineup_container, Game.Side.PLAYER)
+	display_starter_lineups(SceneVariables.cpu_team_bp_configs, cpu_lineup_container, Game.Side.CPU)
+
+func display_starter_lineups(team_bp_configs, container, side: Game.Side):
 	var positions = team_bp_configs.keys()
 	positions.sort()
 	clear_prev_children(container)
 	for p in positions:
 		init_card(container, team_bp_configs[p])
+	var bp_data_cache = SceneVariables.player_team_bp_data_cache if side == Game.Side.PLAYER else SceneVariables.cpu_team_bp_data_cache
+	Game.load_bp_data_cache_for_team(container.get_children(), bp_data_cache)
+
+func display_bench_lineups(bench_lineup, container, side: Game.Side):
+	clear_prev_children(container)
+	for config in bench_lineup:
+		init_card(container, config)
 	var bp_data_cache = SceneVariables.player_team_bp_data_cache if side == Game.Side.PLAYER else SceneVariables.cpu_team_bp_data_cache
 	Game.load_bp_data_cache_for_team(container.get_children(), bp_data_cache)
 
@@ -64,3 +77,18 @@ func on_click_card(bp_card: BallPlayerCard):
 
 func close_bp_card_preview():
 	bp_card_preview.queue_free()
+
+func on_compare_bench():
+	if is_comparing_bench:
+		display_starter_lineups(SceneVariables.player_team_bp_configs, player_lineup_container, Game.Side.PLAYER)
+		display_starter_lineups(SceneVariables.cpu_team_bp_configs, cpu_lineup_container, Game.Side.CPU)
+		cpu_lineup_label.text = "CPU Starting Lineup"
+		player_lineup_label.text = "Player Starting Lineup"
+		compare_lineup_button.text = "Compare Bench"
+	else:
+		display_bench_lineups(SceneVariables.player_team_bench, player_lineup_container, Game.Side.PLAYER)
+		display_bench_lineups(SceneVariables.cpu_team_bench, cpu_lineup_container, Game.Side.CPU)
+		cpu_lineup_label.text = "CPU Bench Lineup"
+		player_lineup_label.text = "Player Bench Lineup"
+		compare_lineup_button.text = "Compare Starting Lineup"
+	is_comparing_bench = !is_comparing_bench
